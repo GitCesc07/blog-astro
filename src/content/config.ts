acebbff15f2402cb820614e3994ad08fad9c1e64
César Urroz <cesar04071997@gmail.com>
2025-08-02 23:07:06 -0600

    Finalizando proyecto

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,5 @@
 import { z } from "astro/zod";
-import { defineCollection } from "astro:content";
+import { defineCollection, reference } from "astro:content";
 
 const  blogCollection = defineCollection ({
     type: "content",
@@ -10,13 +10,32 @@ const  blogCollection = defineCollection ({
         image: image(),
         
         // * Relación
-        author: z.string(),
+        // author: z.string(),
+        author: reference("author"),
 
         // * Relación
-        tags: z.array(z.string())
+        tags: z.array(z.string()),
+
+
+        // * Boolean
+        isDraft: z.boolean().default(false),
+    })
+});
+
+const authorCollection = defineCollection({
+    type: "data",
+    schema: ({ image }) => z.object({
+        name: z.string(),
+        avatar: image(),
+        twitter: z.string(),
+        linkedIn: z.string(),
+        github: z.string(),
+        bio: z.string(),
+        subtitle: z.string(),
     })
 })
 
 export const collections = {
-    blog: blogCollection
+    blog: blogCollection,
+    author: authorCollection
 }
