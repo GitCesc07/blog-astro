8297e122ad7c2f751f10682e17a2d18cae15251e
César Urroz <cesar04071997@gmail.com>
2025-09-21 01:35:11 -0600

    Camnboiando la url de la web

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   },
 
   integrations: [mdx()],
-  site: "https://example.com",
+  site: "https://blogs-cescdev.netlify.app",
 
   image: {
     service: passthroughImageService(),
